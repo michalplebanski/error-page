6b434682292cbe2017df0915df74b7e8abd5e99f
Michal <michalplebanski@gmail.com>
2018-07-04 16:51:20 +0200

    adding js

---
+++ src/script.js
@@ -0,0 +1,10 @@
+var btnMagnifier = $('.magnifier'),
+	btnHome = $('.msg__button--click');
+
+btnMagnifier.click(function() {
+	window.open('https://www.google.com/');
+});
+
+btnHome.click(function() {
+	window.open('https://www.google.com/');
+});
